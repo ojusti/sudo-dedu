2e1cc7045c3f348d8a45281b774e86e3ae1af3eb
Justi Oprescu <j@casesensitive.space>
2017-04-25 22:08:36 +0300

    Empty project

---
+++ src/main/java/space/casesensitive/Sudoku.java
@@ -0,0 +1,9 @@
+package space.casesensitive;
+
+public class Sudoku 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sudoku Solver" );
+    }
+}
---
+++ src/test/java/space/casesensitive/SudokuTest.java
@@ -0,0 +1,14 @@
+package space.casesensitive;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SudokuTest 
+{
+	@Test
+    public void testTheTest()
+    {
+        assertTrue( true );
+    }
+}
